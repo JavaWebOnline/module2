46f30405d2855820ae42a99df48bf6fbe9c22772
ngocquang95 <ngocquang12395@gmail.com>
2023-07-31 14:56:23 +0700

    MyArrayList

---
+++ src/ss7_list/Main.java
@@ -0,0 +1,16 @@
+package ss7_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add(10);
+        myArrayList.add(20);
+        myArrayList.add(30);
+        myArrayList.add(40);
+
+        System.out.println(myArrayList);
+    }
+}
