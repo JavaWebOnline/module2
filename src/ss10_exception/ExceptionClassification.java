f0412a1ca2373661d93076ef04d0af34d9391a13
ngocquang95 <ngocquang12395@gmail.com>
2023-08-25 11:09:03 +0700

    ExceptionClassification

---
+++ src/ss10_exception/ExceptionClassification.java
@@ -0,0 +1,11 @@
+package ss10_exception;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class ExceptionClassification {
+    public static void main(String[] args) {
+//        File file = new File("src/data/student.text");
+//        FileReader fileReader = new FileReader(file);
+    }
+}
